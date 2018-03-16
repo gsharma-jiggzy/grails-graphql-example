d941bdd71fc8fb43e8606fd0586dcd2c12272325
Gaurev Sharma <gsharma@i-a-i.com>
2018-03-16 01:36:29 -0400

    Updated queries

--- grails-app/domain/graphql/Card.groovy
+++ grails-app/domain/graphql/Card.groovy
@@ -3,7 +3,10 @@ package graphql
 import grails.rest.Resource
 import org.grails.gorm.graphql.entity.dsl.GraphQLMapping
 import org.grails.gorm.graphql.entity.dsl.GraphQLPropertyMapping
-
+import graphql.schema.DataFetcher
+import graphql.schema.DataFetchingEnvironment
+import org.grails.gorm.graphql.fetcher.impl.SingleEntityDataFetcher
+import org.grails.gorm.graphql.fetcher.impl.EntityDataFetcher
 
 @Resource(uri='/card')
 class Card {
@@ -18,11 +21,29 @@ class Card {
     String script 
 
     // static graphql = true
-    static graphql = GraphQLMapping.build {
+    static graphql = GraphQLMapping.lazy {
         property('name', description: 'Name of the card')
         property('attack', description: 'Attack Points')
         property('defense', description: 'Defense Points')
         property('type', description: 'Type of card')
+        query('cardByCode', Card) {
+            argument('code', String)
+            dataFetcher(new SingleEntityDataFetcher(Card.gormPersistentEntity) {
+                @Override
+                Object get(DataFetchingEnvironment environment) {
+                    Card.findByCode(environment.getArgument('code'))
+                }
+            })
+        }
+        query('cardByType', [Card]) {
+            argument('type', String)
+            dataFetcher(new EntityDataFetcher(Card.gormPersistentEntity) {
+                @Override
+                Object get(DataFetchingEnvironment environment) {
+                    Card.findAllByType(environment.getArgument('type'))
+                }
+            })
+        }
     }
 
     static constraints = {
